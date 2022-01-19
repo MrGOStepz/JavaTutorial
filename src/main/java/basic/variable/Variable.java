16488aafe0c82bef3f9eb164e31323d2ccf7051a
Warawat Suksakulwat <mrgostepz@gmail.com>
2022-01-19 16:52:33 +0700

    Variable

---
+++ src/main/java/basic/variable/Variable.java
@@ -0,0 +1,26 @@
+package basic.variable;
+
+import java.lang.reflect.Array;
+
+//Reference https://www.geeksforgeeks.org/introduction-to-java/?ref=lbp
+
+public class Variable {
+    public static void main(String[] args) {
+        //Primitive Data Type
+        short sNumber = 0;
+        int number = 0;
+        long lLong = 0L;
+        byte b = 1;
+        char character = 'A';
+        float floating = 0.0F;
+        double doubleType = 0.0d;
+        boolean bool = true;
+
+        //Non-Primitive Data Type or Object Data type
+        String string = "Example";
+        Array array;
+
+
+        System.out.println();
+    }
+}
