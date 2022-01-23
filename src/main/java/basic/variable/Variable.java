f02befe38708d952ac208f7f04358ada37417673
Warawat Suksakulwat <mrgostepz@gmail.com>
2022-01-23 13:06:20 +0700

    Update Variable.java

--- src/main/java/basic/variable/Variable.java
+++ src/main/java/basic/variable/Variable.java
@@ -3,8 +3,24 @@ package basic.variable;
 import java.lang.reflect.Array;
 
 //Reference https://www.geeksforgeeks.org/introduction-to-java/?ref=lbp
+//Name convention https://en.wikipedia.org/wiki/Naming_convention_(programming)#Java
 
+//Name convention camelCase
+//class Tutorial noun
+//function verb
+//variable noun
 public class Variable {
+    //Static Variable
+    static String staticValue = "Static Value";
+    //Instance Variable
+    int number = 0;
+
+    //Scope of Variable
+    //public
+    //protected
+    //default
+    //private
+
     public static void main(String[] args) {
         //Primitive Data Type
         short sNumber = 0;
@@ -20,7 +36,8 @@ public class Variable {
         String string = "Example";
         Array array;
 
-
+        //Local Variables+
+        int localVariable = 0;
         System.out.println();
     }
 }
